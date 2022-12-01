96f3f3aa821d0c1cf510aa7c4ce3d78331e711a5
Mochammad Tedy Fazrin <tedy@jkt1.ebdesk.com>
2022-12-01 18:20:39 +0700

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-facebook-login
+    app-id="687646526313011"
+    @sdk-init="handleSdkInit"
+    @login="handleLogin"
+  ></v-facebook-login>
+</template>
+
+<script>
+import VFacebookLogin from "vue-facebook-login-component";
+
+export default {
+  components: {
+    VFacebookLogin,
+  },
+  data: () => ({
+    FB: {},
+    model: {},
+    scope: {},
+  }),
+  methods: {
+    handleSdkInit({ FB, scope }) {
+      this.FB = FB;
+      this.scope = scope;
+    },
+    handleLogin(model) {
+      console.log(model);
+    },
+  },
+};
+</script>
